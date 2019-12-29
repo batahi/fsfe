5652bd134c3acd9c3718dc000b2b822fadd37a98
abe batahi <abatahi@nvidia.com>
2019-12-28 17:19:24 -0800

    adding one path

--- app.js
+++ app.js
@@ -5,4 +5,8 @@ const port = 3000;
 app.get('/', (req, res)=> {
 	res.send('batahi server');
 });
+
+app.get('/error', (req,res)=> {
+	res.status(418).set('X-Test-Header', 'FSFE').send('error Page');
+})
 app.listen(port, ()=> console.log(`app listerning at ${port}`));
